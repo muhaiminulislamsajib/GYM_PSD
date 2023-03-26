42befe866c32e68ce25dd816e33e627e6b4c2890
Muhaiminul Islam Sajib <muhaiminulislamsajib101@gmail.com>
2023-03-26 16:16:21 +0600

    Add existing project files to Git

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,37 @@
-$(function() {
-    var header = $(".nav_main");
-  
-    $(window).scroll(function() {    
-        var scroll = $(this).scrollTop();
-        if (scroll >= 20) {
-            header.addClass("scrolled");
-        } else {
-            header.removeClass("scrolled");
-        }
+
+$(document).ready(function() {
+    $(function() {
+        var header = $(".nav_main");
+      
+        $(window).scroll(function() {    
+            var scroll = $(this).scrollTop();
+            if (scroll >= 20) {
+                header.addClass("scrolled");
+            } else {
+                header.removeClass("scrolled");
+            }
+        });
+      
     });
-  
-});
+
+    $('.testi_main').slick({
+        autoplay: true,
+        autoplaySpeed: 1000,
+        speed: 600,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+    });
+
+    new VenoBox({
+        selector: '.my-video-links',
+    });
+}); 
 
 
-new VenoBox({
-    selector: '.my-video-links',
+$('.counter').counterUp({
+    delay: 10,
+    time: 1000
 });
