f2a9006838b92ac2c480a7c484a73d78f9d0cea7
Muhaiminul Islam Sajib <muhaiminulislamsajib101@gmail.com>
2023-03-27 08:12:33 +0600

    third Commit

--- js/custom.js
+++ js/custom.js
@@ -9,9 +9,14 @@ $(document).ready(function() {
                 header.addClass("scrolled");
             } else {
                 header.removeClass("scrolled");
+            } 
+            if (scroll > 20){
+                $(".backtotop").fadeIn();
+            } else {
+                $(".backtotop").fadeOut();
             }
         });
-      
+        
     });
 
     $('.testi_main').slick({
@@ -33,5 +38,50 @@ $(document).ready(function() {
 
 $('.counter').counterUp({
     delay: 10,
-    time: 1000
+    time: 10000
 });
+
+$('.sponsor_main').slick({
+    autoplay: true,
+    autoplaySpeed: 1000,
+    speed: 600,
+    infinite: true,
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    nextArrow: ".next",
+    prevArrow: ".prev",
+});
+
+$(".backtotop").click(function(){
+    $("html, body").animate(
+        {
+            scrollTop: 0,
+        },
+        1000
+    )
+});
+
+$(".switch").click (function(){
+   $(".color_switcher").toggleClass("change")
+})
+
+$(".black").click (function(){
+    $(".style").attr("href","css/black.css");
+ });
+
+ $(".blue").click (function(){
+    $(".style").attr("href","css/blue.css");
+ });
+
+// const body = document.querySelector("body");
+// const toggle = document.querySelector("#toggle");
+// const sunIcon = document.querySelector(".toggle .bxs-sun");
+// const moonIcon = document.querySelector(".toggle .bx-moon");
+
+// toggle.addEventListener("change", () => {
+    
+//     body.classList.toggle("dark");
+//     sunIcon.className = sunIcon.className == "bx bxs-sun" ? "bx bx-sun" : "bx bxs-sun";
+//     moonIcon.className = moonIcon.className == "bx bxs-moon" ? "bx bx-moon" : "bx bxs-moon";
+
+// });
